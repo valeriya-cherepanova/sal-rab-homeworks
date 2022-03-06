71dee2f94370b3c410c03e7392f836b84b8523df
Valeriya Cherepanova <valeriya.cherepanova@gmail.com>
2022-03-06 16:16:49 +0300

    задание 1.1-00

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,8 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = productName 'за' productPrice 'теперь в корзине!';
+    console.log(message);
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
