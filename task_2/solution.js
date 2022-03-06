18fa6eec658af9e6c479fc62e093be4759ecc812
Valeriya Cherepanova <valeriya.cherepanova@gmail.com>
2022-03-06 23:10:44 +0300

    2.1-00, 2.2-00, 2.3-00

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,6 +2,14 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
+    let shippingSum;
+
+    if (productsSum == 0) {shippingSum = 0;
+    } else
+        if (productsSum >= freeShippingMinSum) {shippingSum = 0;
+        } else
+            if ((productsSum > 0) && (productsSum < freeShippingMinSum)) {shippingSum = shippingPrice;
+            }
 
     // Задание №2.1. Рассчитать доставку
 
@@ -25,7 +33,12 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
+    let discountSum;
 
+    if (productsSum >= discountMinSum) {discountSum = (productsSum * (discountPart / 100));
+    } else {discountSum = 0;
+      }
+        
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
@@ -42,6 +55,9 @@ function calcDiscount(sum, min, discount) {
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
@@ -51,6 +67,11 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+
+    totalSum = totalSum + shippingSum;
+    let freeShipping;
+    freeShipping = false; // бесплатной доставки по умолчанию нет
+    freeShipping = !shippingSum;
 
     // прибавьте к totalSum значение shippingSum
 
