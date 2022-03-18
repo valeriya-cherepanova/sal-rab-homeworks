e0b262ce2402037cff0c7144abaaaec58c94b346
Valeriya Cherepanova <valeriya.cherepanova@gmail.com>
2022-03-18 21:37:05 +0300

    final task-00

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,19 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: "", order: {address, sum}, goods: []};
+   
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push(goods[i]);
     }
 
+    data.client = name + phone;
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
+    data.order.sum = sum;
+       
     let jsonData = JSON.stringify(data);
 
     return jsonData;
